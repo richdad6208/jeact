b049030d2d2fc73e53d09990e9405a8122402664
Jaesung Park <richdad6208@gmail.com>
2024-12-14 18:16:26 +0900

    Feat: 서버사이드 렌더링

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
-import http from "node:http";
-
-const hostname = "127.0.0.1";
+import express from "express";
+import path from "path";
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, world!\n");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
